8d872ee248ac7effaa18479bfc142d1bb01da013
isox <theo.meunier41@gmail.com>
2020-06-23 20:08:45 +0200

    upgrad

--- views/head.php
+++ views/head.php
@@ -2,5 +2,5 @@
 <title>L'imprimeur </title>
 <meta name="dsicription"content="l'impression 3D et son fonctionnement, tout connaitre sur le
 sujets et appronfondir ces connaissances " >
-<link rel="stylesheet" type="text/css" href="/css/style.css"/>
+<link rel="stylesheet" type="text/css" href="/css/style.min.css"/>
 <link rel="icon"  href ="image/png/logo_isox3.png"/>
