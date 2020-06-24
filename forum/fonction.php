016eaa585a03a8cc69b9931cde751246c0b9c59e
isox <theo.meunier41@gmail.com>
2020-06-24 10:34:44 +0200

    upgrad

--- forum/fonction.php
+++ forum/fonction.php
@@ -1,5 +1,30 @@
 <?php
+require_once '../vendor/autoload.php';
+
 function str_random($length){
     $alphabet="0123456789azertyuiopqsdfghjklmwxcvbnAZERTYUIOPQSDFGHJKLMWXCVBN";
      return substr(str_shuffle(str_repeat($alphabet, $length)), 0, $length);
 }
+function mail()
+{
+    // Create the Transport
+    $transport = (new Swift_SmtpTransport('smtp.example.org', 25))
+        ->setUsername('your username')
+        ->setPassword('your password')
+    ;
+
+// Create the Mailer using your created Transport
+    $mailer = new Swift_Mailer($transport);
+
+// Create a message
+    $message = (new Swift_Message('Wonderful Subject'))
+        ->setFrom(['john@doe.com' => 'John Doe'])
+        ->setTo(['receiver@domain.org', 'other@domain.org' => 'A name'])
+        ->setBody('Here is the message itself')
+    ;
+
+// Send the message
+    $result = $mailer->send($message);
+
+    return $result;
+}};
