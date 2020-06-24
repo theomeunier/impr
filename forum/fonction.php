b65cb900391463351137d1d9a791d49a8f544b4b
isox <theo.meunier41@gmail.com>
2020-06-24 12:47:59 +0200

    upgrad

--- forum/fonction.php
+++ forum/fonction.php
@@ -5,23 +5,29 @@ function str_random($length){
     $alphabet="0123456789azertyuiopqsdfghjklmwxcvbnAZERTYUIOPQSDFGHJKLMWXCVBN";
      return substr(str_shuffle(str_repeat($alphabet, $length)), 0, $length);
 }
-function mail() {
+/*
+ * Envoie d'email via un serveu SMTP
+ * @param $subject
+ * @param array $from
+ * @param array $to
+ * @param $body
+ * @return int
+ */
+function mail($subject, array $from , array $to, $body) {
     // Create the Transport
     $transport = (new Swift_SmtpTransport('in-v3.mailjet.com', 587 ))
         ->setUsername('79fafba77e9118cb2961537bef2b2c27')
         ->setPassword('2ab06fe6a9404acda6567a480afdf773')
     ;
-
 // Create the Mailer using your created Transport
     $mailer = new Swift_Mailer($transport);
 
 // Create a message
-    $message = (new Swift_Message('Wonderful Subject'))
-        ->setFrom(['john@doe.com' => 'John Doe'])
-        ->setTo(['receiver@domain.org', 'other@domain.org' => 'A name'])
-        ->setBody('Here is the message itself')
+    $message = (new Swift_Message($subject))
+        ->setFrom($from)
+        ->setTo($to)
+        ->setBody($body)
     ;
-
 // Send the message
     $result = $mailer->send($message);
 
