fe1c7bb40d18d44066249ce31160da412c9b2cfc
RICHARD Eddy <richard.eddy@live.fr>
2020-06-26 18:01:53 +0200

    Utilisation d'un template commun à toutes les pages du site

--- index.php
+++ index.php
@@ -1,28 +1,19 @@
-<?php include $_SERVER['DOCUMENT_ROOT'] . "/methods/display.php"; ?>
-<!DOCTYPE html>
-<html lang="fr" xml:lang="fr" xmlns="http://www.w3.org/1999/xhtml" xmlns="http://www.w3.org/1999/html">
-<head>
-    <title>l'imprimeur</title>
-    <?php print_head(); ?>
-</head>
-<body>
-<div id="wrapper">
-    <?php print_header(); ?>
-</body>
-<section class="content_wrapper">
-    <h1 class="title"> Bienvenue sur L'imprimeur</h1>
-    <p class="text">
-        Ce site vous propose des aides sur le monde de la 3D<br><br>
-        Ainsi vous pouvez trouver les différentes machines pour la 3D, les différentes matières qui vont à cela.
-        Vous pouvez trouver des solutions pour vos problèmes avec votre imprimante. Ainsi je vous mets à votre
-        disposition.<br><br>
-        Pour bien comprendre votre imprimante je vous conseille de bien l'étudier. Et pourquoi pas l'assembler comment
-        sur certaines imprimante aujourd'hui comme l'Anet A8, Prusa i3 mk3 etc... Pour ainsi bien comprendre
-        comment fonctionne une imprimante, après cela vous trouverais vos problèmes tout seul car vous sauriez
-        comment les résoudre.<br><br>
-        Pour plus d'information je vous conseille d'aller voir les différentes parties que je vous propose ci-dessous.
-    </p>
-</section>
-    <?php print_footer(); ?>
-</div
-</html>
+<?php ob_start(); ?>
+
+<h1 class="title"> Bienvenue sur L'imprimeur</h1>
+<p class="text">
+    Ce site vous propose des aides sur le monde de la 3D<br><br>
+    Ainsi vous pouvez trouver les différentes machines pour la 3D, les différentes matières qui vont à cela.
+    Vous pouvez trouver des solutions pour vos problèmes avec votre imprimante. Ainsi je vous mets à votre
+    disposition.<br><br>
+    Pour bien comprendre votre imprimante je vous conseille de bien l'étudier. Et pourquoi pas l'assembler comment
+    sur certaines imprimante aujourd'hui comme l'Anet A8, Prusa i3 mk3 etc... Pour ainsi bien comprendre
+    comment fonctionne une imprimante, après cela vous trouverais vos problèmes tout seul car vous sauriez
+    comment les résoudre.<br><br>
+    Pour plus d'information je vous conseille d'aller voir les différentes parties que je vous propose ci-dessous.
+</p>
+
+<?php
+    $content = ob_get_clean();
+    include 'template.php';
+?>
