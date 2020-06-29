5cebbc26a8e3468ca9cdb1405e715d45cb27457f
RICHARD Eddy <richard.eddy@live.fr>
2020-06-29 23:10:41 +0200

    syntax comfirme.php

--- forum/confirme.php
+++ forum/confirme.php
@@ -6,17 +6,18 @@ session_abort();
 $user_id = $_GET['id'];
 $token = $_GET['token'];
 
-$req = $pdo -> prepare('SELECT confirmation_token FROM user WHERE id = ?');
+$req = $pdo->prepare('SELECT confirmation_token FROM user WHERE id = ?');
 $req->execute([$user_id]);
-$user = $req-> fetch();
+$user = $req->fetch();
 
 if ($user && $user->confirmation_token == $token) {
      $req = $pdo->prepare('UPDATE user SET confirmation_token = NULL,  confirmation_at = NOW() WHERE id = ?');
-     $req ->execute([$user_id]);
+     $req->execute([$user_id]);
      $_SESSION['auth'] = $user;
 
      header("Location: ../forum/account.php");
 } else {
-    $_SESSION['flash']['danger']=" Ce toker n'est plus valide";
-    header('Location: forum/login.php');
+    $_SESSION['flash']['danger'] = "Ce token n'est plus valide";
+
+    header('Location: index.php');
 }
