f81dd9206e6568fdcddd9e642fd1f971222ef0bb
Théo Meunier <theo.meunier41@gmail.com>
2020-07-01 00:16:06 +0200

    ajout framework

--- template.php
+++ template.php
@@ -20,14 +20,78 @@ if (isset($_SESSION['flash'])) : ?>
     <title>L'imprimeur</title>
     <meta name="dsicription"
           content="l'impression 3D et son fonctionnement, tout connaitre sur le sujets et appronfondir ces connaissances ">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/css/bootstrap.min.css" integrity="sha384-r4NyP46KrjDleawBgD5tp8Y7UzmLA05oM1iAEQ17CSuDqnUK2+k9luXQOfXJCJ4I" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/css/bootstrap.min.css"
+          integrity="sha384-r4NyP46KrjDleawBgD5tp8Y7UzmLA05oM1iAEQ17CSuDqnUK2+k9luXQOfXJCJ4I" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="/css/style.min.css"/>
     <link rel="stylesheet" type="text/css" href="/css/impri.min.css"/>
     <link rel="icon" href="image/png/logo_isox3.png"/>
 </head>
 <body>
-<div id="wrapper">
-    <header>
+<div class="container">
+    <nav class="navbar navbar-expand-lg navbar-light sticky-top" style="background-color: #58a1e5;">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">
+                <img src="/image/Logo_isox1.png" width="30" height="30" class="d-inline-block align-top" alt=""
+                     loading="lazy">
+            </a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+                    aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav mr-auto mb-2 mb-lg-0">
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownImprimante" role="button"
+                           data-toggle="dropdown" aria-expanded="false">
+                            Les imprimantes 3D
+                        </a>
+                        <ul class="dropdown-menu" aria-labelledby="navbarDropdownImprimante">
+                            <li><a class="dropdown-item" href="/imprimante/index.php">Les imprimantes</a></li>
+                            <li>
+                                <hr class="dropdown-divider">
+                            </li>
+                            <li><a class="dropdown-item" href="/imprimante/fdm.php">fdm</a></li>
+                            <li><a class="dropdown-item" href="/imprimante/sla.php">sla</a></li>
+                            <li><a class="dropdown-item" href="/imprimante/polyjet.php">polyjet</a></li>
+                        </ul>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMateriaux" role="button"
+                           data-toggle="dropdown" aria-expanded="false">
+                            Les matériaux
+                        </a>
+                        <ul class="dropdown-menu" aria-labelledby="navbarDropdownMateriaux">
+                            <li><a class="dropdown-item" href="/materiaux/index.php">Matériaux</a></li>
+                            <li>
+                                <hr class="dropdown-divider">
+                            </li>
+                            <li><a class="dropdown-item" href="/materiaux/pla.php">pla</a></li>
+                            <li><a class="dropdown-item" href="/materiaux/abs.php">abs</a></li>
+                            <li><a class="dropdown-item" href="/materiaux/hips.php">hips</a></li>
+                            <li><a class="dropdown-item" href="/materiaux/resines.php">résines</a></li>
+                        </ul>
+                    </li>
+                </ul>
+                <div class="d-flex">
+                    <?php if($_SESSION['auth']): ?>
+                        <div class="btn-group" role="group">
+                            <button id="btnGroupDrop1" type="button" class="btn btn-primary dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
+                                username
+                            </button>
+                            <ul class="dropdown-menu" aria-labelledby="btnGroupDrop1">
+                                <li><a class="dropdown-item" href="forum/account.php">Mon compte</a></li>
+                                <li><a class="dropdown-item" href="forum/logout.php">Se déconnecter</a></li>
+                            </ul>
+                        </div>
+                    <?php else: ?>
+                        <a href="forum/login.php" type="button" class="btn btn-primary">Se connecter</a>
+                    <?php endif; ?>
+                </div>
+            </div>
+        </div>
+    </nav>
+
+    <!-- <header>
         <div>
             <img class="image-header" src="/image/Logo_isox1.png" alt="logo site">
         </div>
@@ -58,7 +122,7 @@ if (isset($_SESSION['flash'])) : ?>
                 <li><a href="/maintenance.php">forum</a></li>
             </ul>
         </nav>
-    </header>
+    </header> -->
     <section class="content_wrapper">
         <?php echo $content; ?>
     </section>
@@ -85,7 +149,11 @@ if (isset($_SESSION['flash'])) : ?>
         </div>
     </footer>
 </div>
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/js/bootstrap.min.js" integrity="sha384-oesi62hOLfzrys4LxRF63OJCXdXDipiYWBnvTl9Y9/TRlw5xlKIEHpNyvvDShgf/" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
+        integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo"
+        crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/js/bootstrap.min.js"
+        integrity="sha384-oesi62hOLfzrys4LxRF63OJCXdXDipiYWBnvTl9Y9/TRlw5xlKIEHpNyvvDShgf/"
+        crossorigin="anonymous"></script>
 </body>
 </html>
