355b9b04e20fe886f020aab95208e4dd793fc13e
RICHARD Eddy <richard.eddy@live.fr>
2020-07-06 20:01:15 +0200

    clean code

--- db.php
+++ db.php
@@ -1,6 +1,6 @@
 <?php
-    require_once $_SERVER['DOCUMENT_ROOT'] . "/variables.php";
+require_once $_SERVER['DOCUMENT_ROOT'] . "/variables.php";
 
-    $pdo = new PDO("mysql:dbname=".DB_NAME.";host=".DB_HOST,DB_USERNAME, DB_PASSWORD);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
+$pdo = new PDO("mysql:dbname=".DB_NAME.";host=".DB_HOST,DB_USERNAME, DB_PASSWORD);
+$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+$pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
